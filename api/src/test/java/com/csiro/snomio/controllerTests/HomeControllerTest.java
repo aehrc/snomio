fa8e4c7eb293350e598936b1ccb9af399802bb2d
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2023-08-01 17:18:20 +1000

    Fix test

--- api/src/test/java/com/csiro/snomio/controllerTests/HomeControllerTest.java
+++ api/src/test/java/com/csiro/snomio/controllerTests/HomeControllerTest.java
@@ -1,10 +1,13 @@
 package com.csiro.snomio.controllerTests;
 
 import com.csiro.snomio.controllers.HomeController;
+import com.csiro.snomio.security.auth.AuthHelper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,6 +20,9 @@ public class HomeControllerTest {
 
   @Autowired
   private MockMvc mvc;
+
+  @MockBean
+  private AuthHelper authHelper;
 
   @Test
   public void getHello() throws Exception {
