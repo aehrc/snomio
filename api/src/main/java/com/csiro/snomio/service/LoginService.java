01244643c64325febbede4e7200f3929abee6028
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2023-08-03 13:44:16 +1000

    Refactoring

--- api/src/main/java/com/csiro/snomio/service/LoginService.java
+++ api/src/main/java/com/csiro/snomio/service/LoginService.java
@@ -1,34 +1,29 @@
 package com.csiro.snomio.service;
 
+import com.csiro.snomio.helper.ApiClientProvider;
 import com.csiro.snomio.models.ImsUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class LoginService {
-  private final String imsCookieName;
-  private final WebClient imsApiClient;
+
+  private final ApiClientProvider apiClientProvider;
 
   @Autowired
-  public LoginService(
-      @Qualifier("imsApiClient") WebClient imsApiClient,
-      @Value("${ims.api.cookie.name}") String imsCookieName) {
-    this.imsApiClient = imsApiClient;
-    this.imsCookieName = imsCookieName;
+  public LoginService(ApiClientProvider apiClientProvider) {
+    this.apiClientProvider = apiClientProvider;
   }
 
   @Cacheable(cacheNames = "users")
   public ImsUser getUserByToken(String cookie) throws AccessDeniedException {
     ImsUser user =
-        imsApiClient
+        apiClientProvider
+            .getImsApiClient(cookie)
             .get()
             .uri("/api/account")
-            .cookie(imsCookieName, cookie)
             .retrieve()
             .bodyToMono(ImsUser.class)
             .block();
--- api/src/main/java/com/csiro/snomio/service/TaskManagerService.java
+++ api/src/main/java/com/csiro/snomio/service/TaskManagerService.java
@@ -1,41 +1,32 @@
 package com.csiro.snomio.service;
 
-import com.csiro.snomio.security.auth.AuthHelper;
+import com.csiro.snomio.helper.ApiClientProvider;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class TaskManagerService {
-  private final WebClient snowStormApiClient;
-  private final String imsCookieName;
-  private final AuthHelper authHelper;
+  private final ApiClientProvider apiClientProvider;
 
   @Autowired
-  public TaskManagerService(
-      @Qualifier("snowStormApiClient") WebClient snowStormApiClient,
-      @Value("${ims.api.cookie.name}") String imsCookieName,
-      AuthHelper authHelper) {
-    this.imsCookieName = imsCookieName;
-    this.snowStormApiClient = snowStormApiClient;
-    this.authHelper = authHelper;
+  public TaskManagerService(ApiClientProvider apiClientProvider) {
+    this.apiClientProvider = apiClientProvider;
   }
 
   public JsonArray getUserTasks() throws AccessDeniedException {
     String json =
-        snowStormApiClient
+        apiClientProvider
+            .getSnowStormApiClient()
             .get()
             .uri("/projects/my-tasks?excludePromoted=false")
-            .cookie(imsCookieName, authHelper.getCookie())
             .retrieve()
-            .bodyToMono(String.class)
+            .bodyToMono(String.class) // TODO May be change to actual objects?
             .block();
-    JsonArray convertedObject = new Gson().fromJson(json, JsonArray.class);
+    JsonArray convertedObject =
+        new Gson().fromJson(json, JsonArray.class); // //TODO Serialization Bean?
     return convertedObject;
   }
 }
