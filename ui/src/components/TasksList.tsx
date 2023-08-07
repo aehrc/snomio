4014f4ce6b58232187005066389e764daada297c
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2023-08-07 21:17:48 +1000

    Task Listing

--- ui/src/components/TasksList.tsx
+++ ui/src/components/TasksList.tsx
@@ -1,29 +1,51 @@
 import useTaskStore from '../stores/TaskStore';
 import TaskItem from './TaskItem';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 interface TaskListProps {
-  listAllTasks?:boolean;
+  listAllTasks?: boolean;
+  heading: string;
 }
-function TasksList({ listAllTasks }: TaskListProps) {
+const columns: GridColDef[] = [
+  { field: 'summary', headerName: 'Name', width: 150 },
+  { field: 'key', headerName: 'Task ID', width: 150 },
+  { field: 'updated', headerName: 'Modified', width: 150 },
+  { field: 'labels', headerName: 'Tickets', width: 150 },
+  { field: 'branchState', headerName: 'Rebase', width: 150 },
+  { field: 'projectKey', headerName: 'Classification', width: 150 },
+  { field: 'latestValidationStatus', headerName: 'Validation', width: 150 },
+  { field: 'feedbackMessagesStatus', headerName: 'Feedback', width: 150 },
+  { field: 'status', headerName: 'Status', width: 150 },
+  {
+    field: 'assignee',
+    headerName: 'Review',
+    width: 150,
+    valueFormatter: ({ value }) => value.displayName,
+  },
+];
+
+function TasksList({ listAllTasks, heading }: TaskListProps) {
   const { tasks, allTasks } = useTaskStore();
-   if(listAllTasks){
-    return (
-      <>
-        {allTasks?.map(task => {
-          return <TaskItem task={task} key={task.key}/>;
-        })}
-      </>
-    );
-   }
+  const taskData = listAllTasks ? allTasks : tasks;
+
   return (
     <>
-      {tasks?.map(task => {
-        return <TaskItem task={task} key={task.key} />;
-      })}
+      <h1>{heading}</h1>
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          getRowId={row => row.key}
+          rows={taskData}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          pageSizeOptions={[5, 10]}
+          //checkboxSelection
+        />
+      </div>
     </>
   );
-
-  
 }
-
 export default TasksList;
