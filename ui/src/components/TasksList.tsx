27825591692049a497892c2de66a6a501b21290f
senjo-aehrc <135300932+senjo-aehrc@users.noreply.github.com>
2023-08-10 14:32:44 +1000

    Feature task listing (#76)
    * Task Listing
    
    * Task Listing
    
    * Task Listing
    
    * Task Listing

--- ui/src/components/TasksList.tsx
+++ ui/src/components/TasksList.tsx
@@ -1,29 +1,79 @@
 import useTaskStore from '../stores/TaskStore';
-import TaskItem from './TaskItem';
+import { DataGrid, GridColDef, GridToolbarQuickFilter } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
 
 interface TaskListProps {
-  listAllTasks?:boolean;
+  listAllTasks?: boolean;
+  heading: string;
 }
-function TasksList({ listAllTasks }: TaskListProps) {
+const columns: GridColDef[] = [
+  { field: 'summary', headerName: 'Name', width: 150 },
+  { field: 'key', headerName: 'Task ID', width: 150 },
+  { field: 'updated', headerName: 'Modified', width: 150 },
+  { field: 'labels', headerName: 'Tickets', width: 150 },
+  { field: 'branchState', headerName: 'Rebase', width: 150 },
+  { field: 'projectKey', headerName: 'Classification', width: 150 },
+  { field: 'latestValidationStatus', headerName: 'Validation', width: 150 },
+  { field: 'feedbackMessagesStatus', headerName: 'Feedback', width: 150 },
+  { field: 'status', headerName: 'Status', width: 150 },
+  {
+    field: 'assignee',
+    headerName: 'Review',
+    width: 150,
+    valueFormatter: ({ value }) => value.displayName,
+  },
+];
+
+function QuickSearchToolbar() {
+  return (
+    <Box
+      sx={{
+        p: 0.5,
+        pb: 0,
+      }}
+    >
+      <GridToolbarQuickFilter
+        quickFilterParser={(searchInput: string) =>
+          searchInput
+            .split(',')
+            .map(value => value.trim())
+            .filter(value => value !== '')
+        }
+      />
+    </Box>
+  );
+}
+
+function TasksList({ listAllTasks, heading }: TaskListProps) {
   const { tasks, allTasks } = useTaskStore();
-   if(listAllTasks){
-    return (
-      <>
-        {allTasks?.map(task => {
-          return <TaskItem task={task} key={task.key}/>;
-        })}
-      </>
-    );
-   }
+  const taskData = listAllTasks ? allTasks : tasks;
+
   return (
     <>
-      {tasks?.map(task => {
-        return <TaskItem task={task} key={task.key} />;
-      })}
+      <Box sx={{ height: 400, width: 1 }}>
+        <h1>{heading}</h1>
+        <DataGrid
+          getRowId={row => row.key}
+          rows={taskData}
+          columns={columns}
+          disableColumnSelector
+          disableDensitySelector
+          slots={{ toolbar: QuickSearchToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            },
+          }}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}
+          //checkboxSelection
+        />
+      </Box>
     </>
   );
-
-  
 }
-
 export default TasksList;
--- ui/src/layouts/TasksLayout.tsx
+++ ui/src/layouts/TasksLayout.tsx
@@ -8,13 +8,11 @@ function TasksLayout() {
 
   useEffect(() => {
     taskStore.fetchAllTasks().catch(err => {
-      console.log(err)
+      console.log(err);
     });
-      taskStore.fetchTasks().catch(err => {
-          console.log(err)
-      });
-
-
+    taskStore.fetchTasks().catch(err => {
+      console.log(err);
+    });
   }, []);
 
   if (taskStore.fetching) {
@@ -22,21 +20,12 @@ function TasksLayout() {
   } else {
     return (
       <Routes>
-        <Route path=""
-          element={
-              <TasksList/>
-          }
+        <Route path="" element={<TasksList heading={'My Tasks'} />} />
+        <Route
+          path="all"
+          element={<TasksList listAllTasks={true} heading={'Tasks'} />}
         />
-          <Route path="all"
-                 element={
-                     <TasksList listAllTasks={true}/>
-                 }
-          />
-        <Route path="edit/:id"
-          element={
-            <>edit</>
-          }
-        />
+        <Route path="edit/:id" element={<>edit</>} />
         {/* not sure about this? Something that chris mentioned - you need to be able to look at the products task?
           dunno how that's different to just a regular task
         */}
