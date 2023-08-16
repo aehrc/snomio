be63e2dc3cdb9dcd6326fec8d6f47ea58a7c43ef
Clinton Gillespie <clintongillespie@outlook.com>
2023-08-17 09:56:21 +1000

    changes to taskslist, made reusable by option to remove pagination etc

--- ui/src/layouts/TaskEditLayout.tsx
+++ ui/src/layouts/TaskEditLayout.tsx
@@ -3,15 +3,27 @@ import { Card, Grid, Tab, Tabs } from '@mui/material';
 
 import useTaskById from '../hooks/useTaskById';
 import TaskEditCard from '../components/tasks/TaskEditCard';
+import TasksList from '../components/TasksList';
+import { Stack } from '@mui/system';
 
 function TaskEditLayout() {
   const task = useTaskById();
 
   return (
     <Grid container sx={{ minHeight: 'calc(100vh - 110px)' }}>
-      <Grid item lg={3} sx={{ maxWidth: '300px' }}>
-        <TaskEditCard />
-      </Grid>
+      <Stack spacing={1} alignItems={'center'} width={'100%'}>
+        <TasksList
+          tasks={task ? [task] : []}
+          heading=""
+          dense={true}
+          naked={true}
+        />
+        <Grid container sx={{ height: '100%' }}>
+          <Grid item lg={3} sx={{ maxWidth: '300px' }}>
+            <TaskEditCard />
+          </Grid>
+        </Grid>
+      </Stack>
     </Grid>
   );
 }
