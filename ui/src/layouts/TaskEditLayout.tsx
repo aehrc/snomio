baf94c99a1c51a7302cac26ab3add3f6f355ab35
Senjo Jose <senjo.kuzhiparambiljose@csiro.au>
2023-08-18 13:53:56 +1000

    Resolve merge conflicts

--- ui/src/layouts/TaskEditLayout.tsx
+++ ui/src/layouts/TaskEditLayout.tsx
@@ -1,85 +1,43 @@
-import { ReactNode, useState } from 'react';
-import { Card, Grid, Tab, Tabs } from '@mui/material';
+import { Grid } from '@mui/material';
 
-import TaskDetails from '../components/tasks/TaskDetails';
 import useTaskById from '../hooks/useTaskById';
-
-interface TabPanelProps {
-  children?: ReactNode;
-  value: number;
-  index: number;
-}
-
-interface TabPanelItem {
-  component: ReactNode;
-}
-const tabPanelItems: TabPanelItem[] = [
-  {
-    component: <div>0</div>,
-  },
-  {
-    component: <div>1</div>,
-  },
-  {
-    component: <div>2</div>,
-  },
-  {
-    component: <TaskDetails />,
-  },
-];
-
-function TabPanel(props: TabPanelProps) {
-  const { value, index } = props;
-
-  return <>{value === index ? <>{tabPanelItems[value].component}</> : <></>}</>;
-}
+import TaskEditCard from '../components/tasks/TaskEditCard';
+import TasksList from '../components/TasksList';
+import TaskTicket from '../components/tasks/TaskTicket';
+import { Stack } from '@mui/system';
+import useTicketStore from '../stores/TicketStore';
+import useJiraUserStore from '../stores/JiraUserStore.ts';
 
 function TaskEditLayout() {
-  const [open, setOpen] = useState(true);
-  const [openTab, setOpenTab] = useState(3);
-
-  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-    event.preventDefault();
-    setOpenTab(newValue);
-  };
+  const task = useTaskById();
+  const { activeTicket } = useTicketStore();
+  const jiraUserStore = useJiraUserStore();
+  const { jiraUsers } = jiraUserStore;
 
   return (
-    <Grid container sx={{ height: '100%!important' }}>
-      <Grid item lg={3} sx={{ height: '100%', maxWidth: '300px' }}>
-        <Card
-          sx={{
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            padding: '1em',
-            maxWidth: '400px',
-          }}
+    <Grid container sx={{ minHeight: 'calc(100vh - 110px)' }}>
+      <Stack spacing={2} alignItems={'center'} width={'100%'}>
+        <TasksList
+          tasks={task ? [task] : []}
+          heading=""
+          dense={true}
+          naked={true}
+          jiraUsers={jiraUsers}
+        />
+        <Stack
+          sx={{ height: '100%', width: '100%' }}
+          direction={'row'}
+          spacing={3}
         >
-          <Tabs
-            variant="scrollable"
-            scrollButtons="auto"
-            textColor="primary"
-            indicatorColor="primary"
-            value={openTab}
-            onChange={handleTabChange}
-            aria-label="Tabs for individual task"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            <Tab label="Search" />
-            <Tab label="List" />
-            <Tab label="Reviews" />
-            <Tab label="Info" />
-          </Tabs>
+          <TaskEditCard />
 
-          <TabPanel index={0} value={openTab} />
-          <TabPanel index={1} value={openTab} />
-          <TabPanel index={2} value={openTab} />
-          <TabPanel index={3} value={openTab} />
-        </Card>
-      </Grid>
+          {activeTicket && (
+            <Grid item lg={3} sx={{}}>
+              <TaskTicket ticket={activeTicket} />
+            </Grid>
+          )}
+        </Stack>
+      </Stack>
     </Grid>
   );
 }
