9a80527324b16bb04acc3848e3ea2b0c1a1b2203
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-08-22 15:41:34 +1000

    test

--- api/src/main/java/com/csiro/snomio/Configuration.java
+++ api/src/main/java/com/csiro/snomio/Configuration.java
@@ -1,10 +1,12 @@
 package com.csiro.snomio;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
+@EntityScan("com.csiro.snomio.models")
 public abstract class Configuration {}
