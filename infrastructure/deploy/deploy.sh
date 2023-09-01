007b7a5005f6d243424a3e81b5c1357a49894ef6
Attila Edelenyi <ede_ati@hotmail.com>
2023-09-01 13:23:57 +1000

    Prepare deployment script to connect to Postgres DB

--- infrastructure/deploy/deploy.sh
+++ infrastructure/deploy/deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ "${DB_USER}" == "" ]; then
+  echo "### ERROR: You must provide a database user via the DB_USER environment variable!"
+  exit 1
+fi
+
+if [ "${DB_PASSWORD}" == "" ]; then
+  echo "### ERROR: You must provide a database password via the DB_PASSWORD environment variable!"
+  exit 1
+fi
+
+
 SNOMIO_ENV=${SNOMIO_ENV:-dev}
 KUBECONFIG=${KUBECONFIG:-~/.kube/config}
 SNOMIO_IMAGE_TAG=${SNOMIO_IMAGE_TAG:-snomio-20230824.1_main}
@@ -7,4 +18,6 @@ HELM_LOCATION=${HELM_LOCATION:-./helm}
 
 helm upgrade --install --kubeconfig ${KUBECONFIG} --namespace snomio-${SNOMIO_ENV} --values snomio-${SNOMIO_ENV}.yaml \
   --set snomio.image="nctsacr.azurecr.io/snomio:${SNOMIO_IMAGE_TAG}" \
+  --set snomio.config."spring\.datasource\.username="="${DB_USER}" \
+  --set snomio.config."spring\.datasource\.password="="${DB_PASSWORD}" \
   --wait --create-namespace snomio-${SNOMIO_ENV} ${HELM_LOCATION}
