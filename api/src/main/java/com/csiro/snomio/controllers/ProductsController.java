957aa267042eba7514a48df568b72edc37e77e19
Dion McMurtrie <dion.mcmurtrie@csiro.au>
2023-09-06 22:28:42 +1000

    added missing dependency, device support, and graph response

--- api/src/main/java/com/csiro/snomio/controllers/ProductsController.java
+++ api/src/main/java/com/csiro/snomio/controllers/ProductsController.java
@@ -1,7 +1,14 @@
 package com.csiro.snomio.controllers;
 
+import com.csiro.snomio.models.product.Edge;
+import com.csiro.snomio.models.product.Node;
 import com.csiro.snomio.models.product.ProductSummary;
 import com.csiro.snomio.service.ProductService;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +28,38 @@ public class ProductsController {
 
   @GetMapping("/{branch}/product-model/{productId}")
   @ResponseBody
-  public ProductSummary tasks(@PathVariable String branch, @PathVariable Long productId) {
+  public ProductSummary getProductModel(@PathVariable String branch, @PathVariable Long productId) {
     return productService.getProductSummary(branch, productId);
+  }
+
+  @GetMapping("/{branch}/product-model-graph/{productId}")
+  @ResponseBody
+  public String getProductModelGraph(@PathVariable String branch,
+      @PathVariable Long productId) {
+
+    ProductSummary summary = productService.getProductSummary(branch, productId);
+
+    Map<String, Set<Node>> nodesByType = new HashMap<>();
+
+    summary.getNodes().forEach(
+        node -> nodesByType.computeIfAbsent(node.getLabel(), k -> new HashSet<>()).add(node));
+
+    StringBuilder graph = new StringBuilder();
+    graph.append("digraph G {\n   rankdir=\"BT\"\n");
+    for (Entry<String, Set<Node>> entry : nodesByType.entrySet()) {
+      graph.append("  subgraph cluster_" + entry.getKey() + " {\n");
+      graph.append("    label = \"" + entry.getKey() + "\";\n");
+      for (Node node : entry.getValue()) {
+        graph.append("    " + node.getConcept().getConceptId() + " [label=\"" + node.getConcept()
+            .getPt().getTerm() + "\"];\n");
+      }
+      graph.append("  }\n");
+    }
+    for (Edge edge : summary.getEdges()) {
+      graph.append("  " + edge.getSource() + " -> " + edge.getTarget() + " [label=\"" + edge
+          .getLabel() + "\" " + (edge.getLabel().equals(ProductService.IS_A_LABEL)
+          ? "arrowhead=empty" : "style=dashed arrowhead=open") + "];\n");
+    }
+    return graph.append("}").toString();
   }
 }
--- api/src/main/java/com/csiro/snomio/service/ProductService.java
+++ api/src/main/java/com/csiro/snomio/service/ProductService.java
@@ -21,10 +21,21 @@ public class ProductService {
   public static final String TPP_FOR_CTPP_ECL = ">> <id> and ^ 929360041000036105";
   public static final String MPP_FOR_CTPP_ECL = ">> <id> and ^ 929360081000036101";
   public static final String TP_FOR_PRODUCT_ECL = ">> <id>.774158006";
-  public static final String TPUU_FOR_CTPP_ECL = "(>> ( <id> .774160008)) and (^ 929360031000036100)";
+  public static final String TPUU_FOR_CTPP_ECL = "(>> ((<id>.774160008) or (<id>.999000081000168101))) and (^ 929360031000036100)";
+  public static final String MPUU_FOR_MPP_ECL = "(>> ((<id>.774160008) or (<id>.999000081000168101))) and (^ 929360071000036103)";
   public static final String MPUU_FOR_TPUU_ECL = ">> <id> and ^ 929360071000036103";
   public static final String MP_FOR_TPUU_ECL = ">> <id> and ^ 929360061000036106";
-  private static final String SUBPACK_FROM_PARENT_PACK_ECL = "<id>.999000011000168107";
+  public static final String CONTAINS_LABEL = "contains";
+  public static final String HAS_PRODUCT_NAME_LABEL = "has product name";
+  public static final String CTPP_LABEL = "CTPP";
+  public static final String TPP_LABEL = "TPP";
+  public static final String MPP_LABEL = "MPP";
+  public static final String IS_A_LABEL = "is a";
+  public static final String TP_LABEL = "TP";
+  public static final String TPUU_LABEL = "TPUU";
+  public static final String MPUU_LABEL = "MPUU";
+  public static final String MP_LABEL = "MP";
+  private static final String SUBPACK_FROM_PARENT_PACK_ECL = "((<id>.999000011000168107) or (<id>.999000111000168106))";
   private final SnowstormClient snowStormApiClient;
 
   @Autowired
@@ -33,12 +44,16 @@ public class ProductService {
   }
 
   public ProductSummary getProductSummary(String branch, Long productId) {
+    // TODO validate productId is a CTPP
+    // TODO handle devices
+    // TODO handle error responses from Snowstorm
+
     ProductSummary productSummary = new ProductSummary();
     addConceptsAndRelationshipsForProduct(branch, productId, productSummary);
     snowStormApiClient.getConceptsFromEcl(branch, SUBPACK_FROM_PARENT_PACK_ECL, productId)
         .forEach(subpack -> {
           addConceptsAndRelationshipsForProduct(branch, subpack.getConceptId(), productSummary);
-          productSummary.addEdge(productId, subpack.getConceptId(), "contains");
+          productSummary.addEdge(productId, subpack.getConceptId(), CONTAINS_LABEL);
         });
 
     DirectedAcyclicGraph<Long, DefaultEdge> graph = new DirectedAcyclicGraph<>(DefaultEdge.class);
@@ -62,33 +77,46 @@ public class ProductService {
   private void addConceptsAndRelationshipsForProduct(String branch, Long productId,
       ProductSummary productSummary) {
     // add the product concept
-    productSummary.addNode(snowStormApiClient.getConcept(branch, productId), "CTPP");
+    productSummary.addNode(snowStormApiClient.getConcept(branch, productId), CTPP_LABEL);
     // add the TPP for the product
-    ConceptSummary tpp = addSingleNode(branch, productSummary, productId, TPP_FOR_CTPP_ECL, "TPP");
+    ConceptSummary tpp = addSingleNode(branch, productSummary, productId, TPP_FOR_CTPP_ECL,
+        TPP_LABEL);
     // look up the MPP for the product summary
-    snowStormApiClient.getConceptsFromEcl(branch, MPP_FOR_CTPP_ECL, productId)
-        .forEach(mpp -> productSummary.addNode(mpp, "MPP"));
+    Collection<ConceptSummary> mpps = snowStormApiClient.getConceptsFromEcl(branch,
+        MPP_FOR_CTPP_ECL, productId);
+    for (ConceptSummary mpp : mpps) {
+      productSummary.addNode(mpp, MPP_LABEL);
+      productSummary.addEdge(tpp.getConceptId(), mpp.getConceptId(), IS_A_LABEL);
+
+      Collection<ConceptSummary> mpuus = snowStormApiClient.getConceptsFromEcl(branch,
+          MPUU_FOR_MPP_ECL, productId);
+      for (ConceptSummary mpuu : mpuus) {
+        productSummary.addNode(mpuu, MPUU_LABEL);
+        productSummary.addEdge(mpp.getConceptId(), mpuu.getConceptId(), CONTAINS_LABEL);
+      }
+    }
+
     // look up the TP
-    ConceptSummary tp = addSingleNode(branch, productSummary, productId, TP_FOR_PRODUCT_ECL, "TP");
-    productSummary.addEdge(tpp.getConceptId(), tp.getConceptId(), "has product name");
+    ConceptSummary tp = addSingleNode(branch, productSummary, productId, TP_FOR_PRODUCT_ECL,
+        TP_LABEL);
+    productSummary.addEdge(tpp.getConceptId(), tp.getConceptId(), HAS_PRODUCT_NAME_LABEL);
 
     // look up TPUUs for the product
     Collection<ConceptSummary> tpuus = snowStormApiClient.getConceptsFromEcl(branch,
-        TPUU_FOR_CTPP_ECL,
-        productId);
+        TPUU_FOR_CTPP_ECL, productId);
     for (ConceptSummary tpuu : tpuus) {
-      productSummary.addNode(tpuu, "TPUU");
-      productSummary.addEdge(tpp.getConceptId(), tpuu.getConceptId(), "contains");
+      productSummary.addNode(tpuu, TPUU_LABEL);
+      productSummary.addEdge(tpp.getConceptId(), tpuu.getConceptId(), CONTAINS_LABEL);
 
       ConceptSummary tpuuTp = addSingleNode(branch, productSummary, tpuu.getConceptId(),
-          TP_FOR_PRODUCT_ECL, "TP");
-      productSummary.addEdge(tpuu.getConceptId(), tpuuTp.getConceptId(), "has product name");
+          TP_FOR_PRODUCT_ECL, TP_LABEL);
+      productSummary.addEdge(tpuu.getConceptId(), tpuuTp.getConceptId(), HAS_PRODUCT_NAME_LABEL);
 
       snowStormApiClient.getConceptsFromEcl(branch, MPUU_FOR_TPUU_ECL, tpuu.getConceptId())
-          .forEach(mpuu -> productSummary.addNode(mpuu, "MPUU"));
+          .forEach(mpuu -> productSummary.addNode(mpuu, MPUU_LABEL));
 
       snowStormApiClient.getConceptsFromEcl(branch, MP_FOR_TPUU_ECL, tpuu.getConceptId())
-          .forEach(mp -> productSummary.addNode(mp, "MP"));
+          .forEach(mp -> productSummary.addNode(mp, MP_LABEL));
     }
   }
 
