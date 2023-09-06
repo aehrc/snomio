057f4c1fd978cc0065d995034ac947e09c31dde5
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-09-07 08:38:17 +1000

    add login tests

---
+++ api/src/test/java/com/csiro/snomio/controllerTests/AuthControllerTest.java
@@ -0,0 +1,28 @@
+package com.csiro.snomio.controllerTests;
+
+import com.csiro.snomio.SnomioTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+class AuthControllerTest extends SnomioTestBase {
+
+  @Test
+  void testLogin() {
+    withAuth()
+        .contentType(ContentType.JSON)
+        .when()
+        .get(this.getSnomioLocation() + "/api/auth")
+        .then()
+        .statusCode(200);
+  }
+
+  @Test
+  void testLoginBadAuth() {
+    withBadAuth()
+        .contentType(ContentType.JSON)
+        .when()
+        .get(this.getSnomioLocation() + "/api/auth")
+        .then()
+        .statusCode(403);
+  }
+}
