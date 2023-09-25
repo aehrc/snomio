28c342a3ba01ea82cb06ed3197daf102e048aa9d
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-09-25 15:12:31 +1000

    formatting

--- api/src/test/java/com/csiro/tickets/repository/TaskAssociationRepositoryTests.java
+++ api/src/test/java/com/csiro/tickets/repository/TaskAssociationRepositoryTests.java
@@ -2,35 +2,24 @@ package com.csiro.tickets.controllers;
 
 import com.csiro.tickets.TicketTestBase;
 import com.csiro.tickets.controllers.dto.TaskAssociationDto;
-import com.csiro.tickets.controllers.dto.TicketDto;
-import com.csiro.tickets.models.State;
 import com.csiro.tickets.models.TaskAssociation;
 import com.csiro.tickets.models.Ticket;
 import com.csiro.tickets.repository.TaskAssociationRepository;
 import com.csiro.tickets.repository.TicketRepository;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import io.restassured.http.ContentType;
 import java.util.List;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class TaskAssociationRepositoryTests extends TicketTestBase {
 
-  @Autowired
-  TaskAssociationRepository taskAssociationRepository;
+  @Autowired TaskAssociationRepository taskAssociationRepository;
 
-  @Autowired
-  TicketRepository ticketRepository;
+  @Autowired TicketRepository ticketRepository;
 
   @Test
-  void getAllTaskAssociations(){
-    Ticket ticket = Ticket.builder()
-        .title("Task association test")
-        .description("A test")
-        .build();
+  void getAllTaskAssociations() {
+    Ticket ticket = Ticket.builder().title("Task association test").description("A test").build();
     Ticket savedTicket = ticketRepository.save(ticket);
 
     TaskAssociation taskAssociation = new TaskAssociation();
