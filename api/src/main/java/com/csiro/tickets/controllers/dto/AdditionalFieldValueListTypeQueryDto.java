60bbce6c4aaa2381eae8d9b1f47fa3e4338d9929
Attila Edelenyi <ede_ati@hotmail.com>
2023-09-29 08:50:02 +1000

    Snoarcloud wanted me to remove this and it's used

--- api/src/main/java/com/csiro/tickets/controllers/dto/AdditionalFieldValueListTypeQueryDto.java
+++ api/src/main/java/com/csiro/tickets/controllers/dto/AdditionalFieldValueListTypeQueryDto.java
@@ -1,8 +1,10 @@
 package com.csiro.tickets.controllers.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AdditionalFieldValueListTypeQueryDto {
 
   private Long typeId;
