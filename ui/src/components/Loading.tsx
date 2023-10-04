ba473bd1b08dcffaeacdc90bca4d404fbb70c0d9
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-10-04 16:28:32 +1000

    removed . . . from loading

--- ui/src/components/Loading.tsx
+++ ui/src/components/Loading.tsx
@@ -1,28 +1,9 @@
 import { CircularProgress, Container, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
 
 interface LoadingProps {
   message?: string;
 }
 function Loading({ message }: LoadingProps) {
-  const [animatedString, setAnimatedString] = useState(message || '');
-  const duration = 1000; // 1000 milliseconds (1 second)
-
-  useEffect(() => {
-    let dotCount = 0;
-    const interval = setInterval(() => {
-      const dots = '.'.repeat(dotCount);
-      setAnimatedString(`${message}${dots}`);
-
-      dotCount++;
-
-      if (dotCount > 3) {
-        dotCount = 0;
-      }
-    }, duration / 3);
-
-    return () => clearInterval(interval); // Cleanup the interval on component unmount
-  }, [message, duration]);
   return (
     <Container
       sx={{
@@ -36,7 +17,7 @@ function Loading({ message }: LoadingProps) {
       }}
     >
       <CircularProgress />
-      <Typography variant="body1">{message && animatedString}</Typography>
+      <Typography variant="body1">{message}</Typography>
     </Container>
   );
 }
