f48915f09aa297ed808901892b7918d945906616
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-10-11 16:19:33 +1000

    renamed the migration file because flyway wasn't into it

--- api/src/main/resources/db/migration/postgresql/V2__additional_field_type.sql
+++ api/src/main/resources/db/migration/postgresql/V2__additional_field_type.sql
@@ -1,14 +1,9 @@
 ALTER TABLE additional_field_type
     ADD type varchar(255);
 
-ALTER TABLE additional_field_type_aud
-    ADD type varchar(255);
-
 UPDATE additional_field_type set type='LIST' where list_type = true;
 
 UPDATE additional_field_type set type='DATE' where LOWER(name) like '%date%';
 
 UPDATE additional_field_type set type='NUMBER' where name = 'ARTGID';
-
-ALTER TABLE additional_field_type drop column list_type;
 
