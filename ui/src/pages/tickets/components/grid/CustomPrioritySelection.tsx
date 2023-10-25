bf9e54bf05177e7d406b747714e021245596197f
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-10-25 14:16:36 +1000

    remove unnecassary console.log

--- ui/src/pages/tickets/components/grid/CustomPrioritySelection.tsx
+++ ui/src/pages/tickets/components/grid/CustomPrioritySelection.tsx
@@ -61,7 +61,7 @@ export default function CustomPrioritySelection({
         });
     }
   };
-  console.log(priorityBucket);
+
   return (
     <Select
       value={priorityBucket?.name ? priorityBucket?.name : ''}
