f0b81a1327cc99431f5ff51d483d4558d507e750
Clinton Gillespie <clinton.gillespie@csiro.au>
2023-11-16 15:23:43 +1000

    temp

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl req -trustout -x509 -nodes -newkey rsa:2048 -keyout ./nginx/key.pem -out ./nginx/cert.pem -sha256 -days 365 \
+openssl req -trustout -x509 -nodes -newkey rsa:2048 -keyout ./snomionginx/key.pem -out ./snomionginx/cert.pem -sha256 -days 365 \
     -subj "/C=AU/ST=QLD/L=Brisbane/O=CSIRO/OU=CSIRO Department/CN=snomio"
 
 docker-compose up
